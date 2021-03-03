cefc5df4593efdf225ec56c58449d347dc19ff1d
Chris Marrison <chris@infoblox.com>
2021-03-03 16:59:41 +0000

    20210303 Restructured

--- dbstream_analysis.py
+++ dbstream_analysis.py
@@ -44,7 +44,7 @@
  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHetreeHER IN CONTRACT, STRICT
  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
@@ -52,51 +52,28 @@
 ------------------------------------------------------------------------
 '''
 __version__ = '0.2.2'
-__author__ = 'John Neerdael'
+__author__ = 'John Neerdael, Chris Marrison'
 __author_email__ = 'jneerdael@infoblox.com'
 
 import argparse, tarfile, logging, re, time, sys, tqdm
 from itertools import (takewhile,repeat)
-import lxml.etree as ET
+from lxml import etree
 
-# Parse arguments
-parser = argparse.ArgumentParser(description='Validate NIOS database backup for B1DDI compatibility',
-                                 prog='xml_analysis')
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
-#parser.add_argument('-d', '--database', action="store", help="Path to database file", required=True)
-parser.add_argument('-c', '--customer', action="store", help="Customer name (optional)")
-options = parser.parse_args()
 
-t = time.perf_counter()
-database = 'okq8.bak'
+def parseargs():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Validate NIOS database backup for B1DDI compatibility')
+    parser.add_argument('-d', '--database', action="store", help="Path to database file", required=True)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s '+ __version__)
+    parser.add_argument('-c', '--customer', action="store", help="Customer name (optional)")
 
-# log events to the log file and to stdout
-dateTime=time.strftime("%H%M%S-%d%m%Y")
-logfile = ''
-if options.customer != '':
-    logfile = f'{options.customer}-{dateTime}.csv'
-else:
-    logfile = f'{dateTime}.csv'
-file_handler = logging.FileHandler(filename=logfile)
-stdout_handler = logging.StreamHandler(sys.stdout)
-# Output to CLI and config
-handlers = [file_handler, stdout_handler]
-# Output to config only
-filehandler = [file_handler]
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(message)s',
-    handlers=filehandler
-)
+    return parser.parse_args()
 
-print('EXTRACTING DATABASE FROM BACKUP')
-tar = tarfile.open(database, "r:gz")
-#tar = tarfile.open(options.database, "r:gz")
-xmlfile = tar.extractfile('onedb.xml')
 
 def rawincount(filename):
     bufgen = takewhile(lambda x: x, (filename.raw.read(1024*1024) for _ in repeat(None)))
     return sum( buf.count(b'\n') for buf in bufgen )
+
 
 def processdhcpoption(xmlobject):
     parent = optiondef = value = ''
@@ -112,6 +89,7 @@ def processdhcpoption(xmlobject):
     hexvalue, optionvalue = validatehex(value)
     return type, parentobj, optionspace, optioncode, hexvalue, optionvalue
 
+
 def processnetwork(xmlobject):
     cidr = address = ''
     for property in xmlobject:
@@ -121,10 +99,11 @@ def processnetwork(xmlobject):
             address = property.attrib['VALUE']
     return address, cidr
 
+
 def validateobject(xmlobject):
     for property in xmlobject.iter():
         if property:
-            print(test)
+            print(etree.tostring(property))
         #for child in property:
             #test = child.get('NAME')
             #print(child.tag, child.attrib)
@@ -147,7 +126,7 @@ def validateobject(xmlobject):
 def checkparentobject(parent):
     objects = re.search(r"(.*)\$(.*)", parent)
     if objects.group(1) == '.com.infoblox.dns.network':
-        type = 'NETWORK'
+        type = 'NetreeWORK'
         parentobj = re.sub(r'\/0$', '', objects.group(2))
         return type, parentobj
     elif objects.group(1) == '.com.infoblox.dns.fixed_address':
@@ -159,11 +138,13 @@ def checkparentobject(parent):
         parentobj = re.sub(r'\/\/\/0\/$', '', objects.group(2))
         return type, parentobj
 
+
 def checkdhcpoption(dhcpoption):
     optioncodes = re.search(r"^(.*)\.\.(true|false)\.(\d+)$", dhcpoption)
     optionspace = optioncodes.group(1)
     optioncode = int(optioncodes.group(3))
     return optionspace, optioncode
+
 
 def validatehex(values):
     if re.search(r"^[0-9a-fA-F:\s]*$", values):
@@ -179,7 +160,10 @@ def validatehex(values):
         hexvalue = False
         return hexvalue, values
 
+
 def validatedhcpoption(type, parentobj, optionspace, optioncode, hexvalue, optionvalue):
+    incompatible_options = [ 12, 124, 125, 146, 159, 212 ]
+    validate_options = [ 43, 151 ]
     if (optioncode == 12 or optioncode == 124 or optioncode == 125 or optioncode == 146 or optioncode == 159 or optioncode == 212):
         logging.info('DHCPOPTION,INCOMPATIBLE,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue)
     elif optioncode == 43:
@@ -192,44 +176,84 @@ def validatedhcpoption(type, parentobj, optionspace, optioncode, hexvalue, optio
     else:
         None
 
+
 def validatenetwork(address, cidr):
     if cidr == '32':
-        logging.info('DHCPNETWORK,INCOMPATIBLE,' + address + '/' + cidr)
+        logging.info('DHCPNetreeWORK,INCOMPATIBLE,' + address + '/' + cidr)
     else:
         None
 
+
 def searchrootobjects(xmlfile, iterations):
+    # parser = etree.XMLPullParser(target=AttributeFilter())
     with tqdm.tqdm(total=iterations) as pbar:
         count = 0
-        xmlfile.seek(0)
-        for event, elem in ET.iterparse(xmlfile):
-            if event == 'end':
+        #xmlfile.seek(0)
+        for event, elem in etree.iterparse(xmlfile):
+            if event == 'start':
                 if elem.tag == 'OBJECT':
+                    print(etree.tostring(elem))
                     count += 1
                     pbar.update(1)
-                    validateobject(elem)
+                    validateobject(etree.ElementTree(elem))
             elem.clear()
+
 
 def writeheaders():
     logging.info('HEADER-DHCPOPTION,STATUS,OBJECTTYPE,OBJECT,OPTIONSPACE,OPTIONCODE,OPTIONVALUE')
-    logging.info('HEADER-DHCPNETWORK,STATUS,OBJECT')
+    logging.info('HEADER-DHCPNetreeWORK,STATUS,OBJECT')
+
 
 def main():
-    t2 = time.perf_counter() - t
-    print(f'EXTRACTED DATABASE FROM BACKUP IN {t2:0.2f}S')
-    iterations = rawincount(xmlfile)
-    xmlfile.seek(0)
-    t3 = time.perf_counter() - t2
-    print(f'COUNTED {iterations} OBJECTS IN {t3:0.2f}S')
-    writeheaders()
-    searchrootobjects(xmlfile, iterations)
-    t4 = time.perf_counter() - t
-    print(f'FINISHED PROCESSING IN {t4:0.2f}S, LOGFILE: {logfile}')
+    '''
+    Core logic
+    '''
+    logfile = ''
+    options = parseargs()
+    t = time.perf_counter()
+    database = options.database
+
+    # Set up logging
+    # log events to the log file and to stdout
+    dateTime=time.strftime("%H%M%S-%d%m%Y")
+    if options.customer != '':
+        logfile = f'{options.customer}-{dateTime}.csv'
+    else:
+        logfile = f'{dateTime}.csv'
+    file_handler = logging.FileHandler(filename=logfile)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    # Output to CLI and config
+    handlers = [file_handler, stdout_handler]
+    # Output to config only
+    filehandler = [file_handler]
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(message)s',
+        handlers=filehandler
+    )
+
+    # Extract db from backup
+    print('EXTRACTING DATABASE FROM BACKUP')
+    with tarfile.open(database, "r:gz") as tar:
+        #tar = tarfile.open(options.database, "r:gz")
+        xmlfile = tar.extractfile('onedb.xml')
+        t2 = time.perf_counter() - t
+        print(f'EXTRACTED DATABASE FROM BACKUP IN {t2:0.2f}S')
+
+        iterations = rawincount(xmlfile)
+        xmlfile.seek(0)
+        t3 = time.perf_counter() - t2
+        print(f'COUNTED {iterations} OBJECTS IN {t3:0.2f}S')
+        writeheaders()
+        searchrootobjects(xmlfile, iterations)
+        t4 = time.perf_counter() - t
+        print(f'FINISHED PROCESSING IN {t4:0.2f}S, LOGFILE: {logfile}')
+
     return
 '''
     count = 0
     xmlfile.seek(0)
-    for event, elem in ET.iterparse(xmlfile):
+    for event, elem in etree.iterparse(xmlfile):
         if event == 'end':
             if elem.tag == 'OBJECT':
                 count += 1
@@ -239,5 +263,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-tar.close()
