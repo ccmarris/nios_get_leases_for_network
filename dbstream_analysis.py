d90ea7d2299a65dace712a0c2310bee113f28132
John <john@neerdael.nl>
2021-03-05 17:40:57 +0100

    Further worked on the forking to a iterparse version and remove old things from the legacy related to that from that branch.

--- dbstream_analysis.py
+++ dbstream_analysis.py
@@ -1,16 +1,12 @@
 #!/usr/local/bin/python3
 '''
 ------------------------------------------------------------------------
-
  Description:
    Python script to search for feature gaps between NIOS and BloxOne DDI
-
  Requirements:
    Python3 with lxml, argparse, tarfile, logging, re, time, sys, tqdm
-
  Author: John Neerdael
-
- Date Last Updated: 20210227
+ Date Last Updated: 20210305
 
  Changelog:
  - 0.1.0: Created new definitions to allow support for more objects and moved logfile to a CSV file
@@ -19,23 +15,22 @@
  - 0.2.2: Clean-up old code snippets
  - 0.2.3: Added demo code for using iterparse instead of parse (stream vs load database to memory)
  - 0.3.0: Forked to seperate version for testing iterparse
+ - 0.3.1: First production iterparse version, added support for networkcontainers, merged fork from Chris, added support for --debug flag
 
  ToDo:
- - Move code to use a iterparse loop for increased efficiency and memory usage and test speed on big databases
+ - Add support for lease object counting
+ - Add support for ini file (Chris)
+ - Add support for DDNS domain analysis
 
  Copyright (c) 2021 John Neerdael / Infoblox
-
  Redistribution and use in source and binary forms,
  with or without modification, are permitted provided
  that the following conditions are met:
-
  1. Redistributions of source code must retain the above copyright
  notice, this list of conditions and the following disclaimer.
-
  2. Redistributions in binary form must reproduce the above copyright
  notice, this list of conditions and the following disclaimer in the
  documentation and/or other materials provided with the distribution.
-
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -48,10 +43,9 @@
  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-
 ------------------------------------------------------------------------
 '''
-__version__ = '0.2.6'
+__version__ = '0.3.1'
 __author__ = 'John Neerdael, Chris Marrison'
 __author_email__ = 'jneerdael@infoblox.com'
 
@@ -66,6 +60,7 @@ def parseargs():
     parser.add_argument('-d', '--database', action="store", help="Path to database file", required=True)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s '+ __version__)
     parser.add_argument('-c', '--customer', action="store", help="Customer name (optional)")
+    parser.add_argument('--debug', help="Enable debug logging", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
 
     return parser.parse_args()
 
@@ -105,30 +100,34 @@ def validateobject(xmlobject):
     for property in xmlobject:
         if property.attrib['NAME'] == '__type' and property.attrib['VALUE'] == '.com.infoblox.dns.option':
             object = 'dhcpoption'
-            log.debug("Type = {}, Attirb = {}".format(object, property.attrib))
+            break
         elif property.attrib['NAME'] == '__type' and property.attrib['VALUE'] == '.com.infoblox.dns.network':
             object = 'dhcpnetwork'
-            log.debug("Type = {}, Attirb = {}".format(object, property.attrib))
-        elif property.attrib['NAME'] == '__type' and property.attrib['VALUE'] != '.com.infoblox.dns.option':
+            break
+        elif property.attrib['NAME'] == '__type':
             object = ''
-
+            break
     return object
 
 
 def checkparentobject(parent):
     objects = re.search(r"(.*)\$(.*)", parent)
+    type = ''
+    parentobj = ''
     if objects.group(1) == '.com.infoblox.dns.network':
-        type = 'NetreeWORK'
+        type = 'NETWORK'
         parentobj = re.sub(r'\/0$', '', objects.group(2))
-        return type, parentobj
     elif objects.group(1) == '.com.infoblox.dns.fixed_address':
         type = 'FIXEDADDRESS'
         parentobj = re.sub(r'\.0\.\.$', '', objects.group(2))
-        return type, parentobj
     elif objects.group(1) == '.com.infoblox.dns.dhcp_range':
         type = 'DHCPRANGE'
         parentobj = re.sub(r'\/\/\/0\/$', '', objects.group(2))
-        return type, parentobj
+    elif objects.group(1) == '.com.infoblox.dns.network_container':
+        type = 'NETWORKCONTAINER'
+        parentobj = re.sub(r'\/0$', '', objects.group(2))
+
+    return type, parentobj
 
 
 def checkdhcpoption(dhcpoption):
@@ -153,26 +152,26 @@ def validatehex(values):
         return hexvalue, values
 
 
-def validatedhcpoption(type, parentobj, optionspace, optioncode, hexvalue, optionvalue):
+def validatedhcpoption(type, parentobj, optionspace, optioncode, hexvalue, optionvalue, count):
     incompatible_options = [ 12, 124, 125, 146, 159, 212 ]
     validate_options = [ 43, 151 ]
     if optioncode in incompatible_options:
-        logging.info('DHCPOPTION,INCOMPATIBLE,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue)
+        logging.info('DHCPOPTION,INCOMPATIBLE,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue + ',' + str(count))
     elif optioncode in validate_options:
         if optioncode == 151:
-            logging.info('DHCPOPTION,VALIDATION_NEEDED,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue)
+            logging.info('DHCPOPTION,VALIDATION_NEEDED,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue + ',' + str(count))
         elif optioncode == 43:
             if hexvalue == True:
-                logging.info('DHCPOPTION,VALIDATION_NEEDED,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue)
+                logging.info('DHCPOPTION,VALIDATION_NEEDED,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue + ',' + str(count))
             elif hexvalue == False:
-                logging.info('DHCPOPTION,INCOMPATIBLE,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue)
+                logging.info('DHCPOPTION,INCOMPATIBLE,' + type + ',' + parentobj + ',' + optionspace + ',' + str(optioncode) + ',' + optionvalue + ',' + str(count))
     else:
         None
 
 
-def validatenetwork(address, cidr):
+def validatenetwork(address, cidr, count):
     if cidr == '32':
-        logging.info('DHCPNetreeWORK,INCOMPATIBLE,' + address + '/' + cidr)
+        logging.info('DHCPNETWORK,INCOMPATIBLE,' + address + '/' + cidr + ',' + str(count))
     else:
         None
 
@@ -190,10 +189,10 @@ def searchrootobjects(xmlfile, iterations):
                     object = validateobject(elem)
                     if object == 'dhcpoption':
                         type, parentobj, optionspace, optioncode, hexvalue, optionvalue = processdhcpoption(elem)
-                        validatedhcpoption(type, parentobj, optionspace, optioncode, hexvalue, optionvalue)
+                        validatedhcpoption(type, parentobj, optionspace, optioncode, hexvalue, optionvalue, count)
                     elif object == 'dhcpnetwork':
                         address, cidr = processnetwork(elem)
-                        validatenetwork(address, cidr)
+                        validatenetwork(address, cidr, count)
                     else:
                         None
                 except:
@@ -205,7 +204,7 @@ def searchrootobjects(xmlfile, iterations):
 
 def writeheaders():
     logging.info('HEADER-DHCPOPTION,STATUS,OBJECTTYPE,OBJECT,OPTIONSPACE,OPTIONCODE,OPTIONVALUE')
-    logging.info('HEADER-DHCPNetreeWORK,STATUS,OBJECT')
+    logging.info('HEADER-DHCPNETWORK,STATUS,OBJECT,OBJECTLINE')
     return
 
 
@@ -232,15 +231,15 @@ def main():
     # Output to config only
     filehandler = [file_handler]
     logging.basicConfig(
-        level=logging.INFO,
+        level=options.loglevel,
         format='%(message)s',
         handlers=filehandler
     )
 
     # Extract db from backup
     print('EXTRACTING DATABASE FROM BACKUP')
+
     with tarfile.open(database, "r:gz") as tar:
-        #tar = tarfile.open(options.database, "r:gz")
         xmlfile = tar.extractfile('onedb.xml')
         t2 = time.perf_counter() - t
         print(f'EXTRACTED DATABASE FROM BACKUP IN {t2:0.2f}S')
@@ -255,16 +254,6 @@ def main():
         print(f'FINISHED PROCESSING IN {t4:0.2f}S, LOGFILE: {logfile}')
 
     return
-'''
-    count = 0
-    xmlfile.seek(0)
-    for event, elem in etree.iterparse(xmlfile):
-        if event == 'end':
-            if elem.tag == 'OBJECT':
-                count += 1
-        elem.clear()
-    print(count)
-'''
 
 if __name__ == '__main__':
     main()
